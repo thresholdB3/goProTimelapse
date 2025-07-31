cf3f819c6a602088fd406c78366cc2fb166a7c3f
threshold <devmagician3@gmail.com>
2025-07-31 18:07:03 +0500

    переименовала dfsdf.csproj; добавила подключение к wifi камеры и обратно к основному wifi

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Diagnostics;
 
 class Program
 {
@@ -16,6 +17,13 @@ class Program
         string mediaHtml = @"href=""/videos/DCIM/100GOPRO/";
         string downloadFolder = "GoProPhotos";
         int photoCount = 5;
+        string ssid1 = "vivo Y22"; //имя основного wifi
+        string password1 = "levynosok0"; //его пароль
+        string camera_ssid = "GP53119127";
+        string camera_password = "p=n-6VT-6Kn";
+
+        ConnectWiFi(camera_ssid, camera_password);
+        await Task.Delay(3000); //3 секунды на подключение
 
         //смена режима на фото; 0 - видео, 1 - фото, 2 - таймлапс
         await client.GetAsync(baseUrl + "/gp/gpControl/command/mode?p=1");
@@ -30,8 +38,7 @@ class Program
             await downloadPhoto();
         }
 
-        //команды на отключение от камеры я не нашла, так что просто выключаем ¯\_(ツ)_/¯
-        await client.GetAsync(baseUrl + "/gp/gpControl/command/system/sleep");
+        ConnectWiFi(ssid1, password1);
 
         async Task takePhoto()
         {
@@ -42,7 +49,7 @@ class Program
 
         async Task downloadPhoto()
         {
-            //получение последних 5 фото
+            //получение последнего фото
 
             //поиск файлов
             var folderHtml = await client.GetStringAsync(folderUrl);
@@ -63,6 +70,67 @@ class Program
 
             //удаление файла
             await client.GetAsync("http://10.5.5.9/gp/gpControl/command/storage/delete/last");
+        }
+        void ExecuteNetshCommand(string arguments)
+        {
+            //создание процесса
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "netsh",
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            // Console.WriteLine(output);
+        }
+        string GenerateXml(string ssid, string password)
+        {
+            return $@"<?xml version=""1.0""?>
+        <WLANProfile xmlns=""http://www.microsoft.com/networking/WLAN/profile/v1"">
+        <name>{ssid}</name>
+        <SSIDConfig>
+            <SSID>
+            <name>{ssid}</name>
+            </SSID>
+        </SSIDConfig>
+        <connectionType>ESS</connectionType>
+        <connectionMode>auto</connectionMode>
+        <MSM>
+            <security>
+            <authEncryption>
+                <authentication>WPA2PSK</authentication>
+                <encryption>AES</encryption>
+                <useOneX>false</useOneX>
+            </authEncryption>
+            <sharedKey>
+                <keyType>passPhrase</keyType>
+                <protected>false</protected>
+                <keyMaterial>{password}</keyMaterial>
+            </sharedKey>
+            </security>
+        </MSM>
+        </WLANProfile>";
+        }
+        void ConnectWiFi(string ssid, string password)
+        {
+            //создание и сохранение xml
+            string xmlProfile = GenerateXml(ssid, password);
+            string tempPath = Path.Combine(Path.GetTempPath(), $"{ssid}_profile.xml");
+
+            File.WriteAllText(tempPath, xmlProfile);
+
+            ExecuteNetshCommand($"wlan add profile filename=\"{tempPath}\"");
+            ExecuteNetshCommand($"wlan connect name=\"{ssid}\" ssid=\"{ssid}\"");
+
+            File.Delete(tempPath);
         }
     }
 }
