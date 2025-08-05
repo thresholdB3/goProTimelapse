44662207413ca40be19decd4bfa1646b6ae47b82
threshold <devmagician3@gmail.com>
2025-08-05 19:13:44 +0500

    добавила время начала и конца таймлапса, расчёт времени съёмки и количества фото; добавила создание таймлапса из фото

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Diagnostics;
+using System.Transactions;
+using System.Text;
+
 
 class Program
 {
@@ -16,11 +19,15 @@ class Program
         string folderUrl = "http://10.5.5.9:8080/videos/DCIM/100GOPRO/";
         string mediaHtml = @"href=""/videos/DCIM/100GOPRO/";
         string downloadFolder = "GoProPhotos";
-        int photoCount = 5;
+        
         string ssid1 = "vivo Y22"; //имя основного wifi
         string password1 = "levynosok0"; //его пароль
         string camera_ssid = "GP53119127";
         string camera_password = "p=n-6VT-6Kn";
+        //здесь будет время начала и конца заката
+        DateTime timeStart = DateTime.Now;
+        DateTime timeStop = new DateTime(2025, 8, 5, 14, 30, 00);//год, месяц, день, часы, минуты, секунды
+        int photoDelay = 15;
 
         ConnectWiFi(camera_ssid, camera_password);
         await Task.Delay(3000); //3 секунды на подключение
@@ -32,22 +39,27 @@ class Program
 
         await Task.Delay(3000); //3 секунды на переключение режима
 
+        int photoCount = getPhotoCount();
+
         for (int i = 0; i < photoCount; i++)
         {
             await takePhoto();
-            await downloadPhoto();
+            await downloadPhoto(i.ToString() + ".jpg");
+            await Task.Delay(photoDelay - 11);//в коде уже есть задержка на 11 секунд
         }
 
         ConnectWiFi(ssid1, password1);
 
+        createTimelapse(@"C:\rrr\csproektete\dfsdf\ogo.mp4");
+
+
         async Task takePhoto()
         {
             await client.GetAsync(baseUrl + "/gp/gpControl/command/shutter?p=1");
-            await Task.Delay(3000); //3 секунды на фото
-            await Task.Delay(3000); //3 секунды на сохранение
+            await Task.Delay(5000); //5 секунд на фото и сохранение
         }
 
-        async Task downloadPhoto()
+        async Task downloadPhoto(string fileName)
         {
             //получение последнего фото
 
@@ -62,7 +74,7 @@ class Program
 
             //загрузка файла
             var fileUrl = $"{mediaUrl}/{file}";
-            var fileName = Path.GetFileName(file);
+            // var fileName = Path.GetFileName(file);
             var localPath = Path.Combine(downloadFolder, fileName);
             var data = await client.GetByteArrayAsync(fileUrl);
             await File.WriteAllBytesAsync(localPath, data);
@@ -131,6 +143,70 @@ class Program
             ExecuteNetshCommand($"wlan connect name=\"{ssid}\" ssid=\"{ssid}\"");
 
             File.Delete(tempPath);
+        }
+        int getPhotoCount()
+        {
+            TimeSpan shootingTime = timeStop - timeStart;
+            Console.WriteLine($"Время съёмки: {shootingTime}");
+            int secondsShootingTime = (int)shootingTime.TotalSeconds;
+
+            //количество фото, которые нужно сделать
+            return secondsShootingTime / photoDelay;
+        }
+        void createTimelapse(string outputFilePath, int outputFps = 25)
+        {
+            //путь к папке проекта и остальные пути
+            string projectRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\.."));
+            string photosDirectory = Path.Combine(projectRoot, "GoProPhotos");
+            string ffmpegPath = Path.Combine(AppContext.BaseDirectory, "ffmpeg.exe");
+
+            var imageFiles = Directory.GetFiles(photosDirectory, "*.jpg");
+
+            //сортировка фото и создание списка
+            var sortedImageFiles = imageFiles
+                .Select(f => new
+                {
+                    Path = f,
+                    Number = int.TryParse(Path.GetFileNameWithoutExtension(f), out int num) ? num : -1
+                })
+                .Where(x => x.Number != -1)
+                .OrderBy(x => x.Number)
+                .Select(x => x.Path)
+                .ToList();
+
+            string inputListPath = Path.Combine(AppContext.BaseDirectory, "input.txt");
+            File.WriteAllLines(inputListPath, sortedImageFiles.Select(f => $"file '{f.Replace("'", @"'\''")}'"));
+            
+            //сложные аргументы для запуска ffmpeg
+            string arguments = $"-f concat -safe 0 -i \"{inputListPath}\" -c:v libx264 -r {outputFps} -pix_fmt yuv420p \"{Path.Combine(projectRoot, DateTime.Now.ToString("ssmmhh.ddMMyyyy"))}.mp4\"";
+
+            //сам запуск
+            using (var process = new Process())
+            {
+                var outputBuilder = new StringBuilder();
+                var errorBuilder = new StringBuilder();
+
+                process.StartInfo.FileName = ffmpegPath;
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                //обработчики для чтения вывода
+                process.OutputDataReceived += (sender, e) => outputBuilder.AppendLine(e.Data);
+                process.ErrorDataReceived += (sender, e) => errorBuilder.AppendLine(e.Data);
+
+                process.Start();
+                //чтение вывода и ошибок, без этого всё зависает
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                process.WaitForExit();
+
+            }
+
+            File.Delete(inputListPath);
         }
     }
 }
